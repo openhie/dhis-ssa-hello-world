47cca27f6e7faa11eb5db192a20bdbf961971d32
Carl Leitner <litlfred@gmail.com>
2016-10-25 12:09:11 -0400

    set content type on hello world

--- ssa/index.js
+++ ssa/index.js
@@ -1,5 +1,6 @@
 
 function display(content) {
+    dhisScriptContext.httpResponse.setContentType("text/html");
     var html="<html><body><div>";
     html += content;
     html +=  '</div></body></html>';
